b7f40981b086c6b9aecdafcfee3a8ab2eca7e5b3
Mohamad Farkhan <67281132+mohamadfarkhan@users.noreply.github.com>
2024-11-10 01:26:38 +0700

    Update Pengerjaan Submission 10/11/2024

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-<<<<<<< HEAD
 import daStyle from "eslint-config-dicodingacademy";
-=======
-import daStyle from 'eslint-config-dicodingacademy';
->>>>>>> origin/main
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   daStyle,
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
 ];
